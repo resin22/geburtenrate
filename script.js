f8a3ad8d2f0a5a38420d1697ed383757f149e6b5
anirbas7 <91332683+anirbas7@users.noreply.github.com>
2023-05-29 16:56:15 +0200

    fourth commit

--- script.js
+++ script.js
@@ -29,42 +29,11 @@ const cantonsLookUp = {
 
 function getDataForYear(data, year) {
     yearData = []
-
     for (let row of data) {
         if (row.YEAR == year) {
             yearData.push(row)
         }
     }
-    return yearData
-}
-
-var $slider = $("#slider");
-var $fill = $(".bar .fill");
-
-function setBar() {
-    yearSlider = ''
-    $fill.css("width", $slider.val())
-    yearSlider = $slider.val()
-    console.log(yearSlider)
-    return yearSlider
-}
-
-(async () => {
-
-    const topology = await fetch(
-        'https://code.highcharts.com/mapdata/countries/ch/ch-all.topo.json'
-    ).then(response => response.json());
-
-    const d = await fetch(
-        'Kantone_AlterMutter.json'
-    ).then(response => response.json());
-
-    $slider.on("input", setBar);
-    setBar();
-
-    yearData = getDataForYear(d, yearSlider)
-    console.log(yearData)
-
     let result = {}
     for (const [key, value] of Object.entries(cantonsLookUp)) {
         result[key] = { 'hc-key': value }
@@ -80,36 +49,125 @@ function setBar() {
             result[row.CANTON].male = row.OBS_VALUE
         }
     }
+    return Object.values(result)
+}
+let countsForYearFemal = {}
+let countsForYearMal = {}
+
+function countTotalValuesForYear(data) {
+    for (let row of data) {
+        if (row.SEX_CHILD == "F") {
+            countsForYearFemal[row.YEAR] = row.OBS_VALUE
+        }
+        if (row.SEX_CHILD == "M") {
+            countsForYearMal[row.YEAR] = row.OBS_VALUE
+        }
+    }
+    return [countsForYearFemal, countsForYearMal]
+}
+
+function getDataForYearBalken(dataB) {
+    resultBalken = {}
+
+    for (let row of dataB) {
+        if (row.Alter_Mutter == "Total") {
+            resultBalken[row.Jahr] = row.Anzahl
+        }
+    }
+    return resultBalken
+}
+
+var $slider = $("#slider");
+var $fill = $(".bar .fill");
+
+function setBar() {
+    yearSlider = ''
+    $fill.css("width", $slider.val())
+    yearSlider = $slider.val()
+    data = getDataForYear(dataKarte, yearSlider)
+    // verändert die Chartdarstellung bei jeder väränderung des Sliders
+    if (chart) {
+        chart.series[0].update({
+            data: data
+        }, true);
+        chart.title.update({
+            text: 'Geburtenzahl in der Schweiz pro Kanton im Jahr ' + yearSlider
+        })
+    }
+    return yearSlider
+}
+resultFrau = {}
+resultErsatz = {}
+
+function getDataForYearLine(dataL) {
+    for (let row of dataL) {
+        if (row.Durchschnitt_Frau != "") {
+            resultFrau[row.Jahr] = row.Durchschnitt_Frau
+        }
+        if (row.Ersatz != "") {
+            resultErsatz[row.Jahr] = row.Ersatz
+        }
+    }
+    return [resultFrau, resultErsatz]
+}
+
+let dataKarte;
+let dataBalken;
+let chart;
+let chartBalken;
+let chartLinie;
+
+(async () => {
+    const topology = await fetch(
+        'https://code.highcharts.com/mapdata/countries/ch/ch-all.topo.json'
+    ).then(response => response.json());
+
+    dataKarte = await fetch(
+        'Kantone_AlterMutter.json'
+    ).then(response => response.json());
+
+    dataLinie = await fetch(
+        'Elterngenerationsersatz.json'
+    ).then(response => response.json());
+    console.log(dataLinie)
+
+    dataBalken = await fetch(
+        'totgeburten.json'
+    ).then(response => response.json());
+
+    $slider.on("input", setBar);
+    setBar();
+
+    test = countTotalValuesForYear(dataKarte)
+    console.log(test)
 
 
-    console.log(result)
+    data = getDataForYear(dataKarte, yearSlider)
+    dataL = getDataForYearLine(dataLinie)
+    console.log(dataL)
+    dataB = getDataForYearBalken(dataBalken)
+    console.log(dataB)
 
-    const data = Object.values(result)
-
-    Highcharts.mapChart('container', {
+    chart = Highcharts.mapChart('container', {
         chart: {
-            map: topology
+            map: topology,
+            animation: false
         },
-
         title: {
-            text: 'Geburtenzahl in der Schweiz pro Kanton im Jahr'
+            text: 'Geburtenzahl in der Schweiz pro Kanton im Jahr 1969'
         },
-
         subtitle: {
             text: 'Source data: <a href="https://www.bfs.admin.ch/bfs/de/home/statistiken/bevoelkerung/geburten-todesfaelle/geburten.html">Bundesamt für Statistik</a>'
         },
-
         mapNavigation: {
             enabled: true,
             buttonOptions: {
                 verticalAlign: 'bottom'
             }
         },
-
         colorAxis: {
             min: 0
         },
-
         series: [{
             data: data,
             name: 'Anzahl Geburten im Jahr pro Kanton',
@@ -132,9 +190,95 @@ function setBar() {
                 return `Gesammtanzahl: ${total}, Mädchen: ${female}, Knaben: ${male}`
             }
         }
-
     });
 
+    chartLinie = Highcharts.chart('lineContainer', {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Ersatz der Elterngeneration'
+        },
+        subtitle: {
+            text: 'Source: <a href="https://www.bfs.admin.ch/bfs/de/home/statistiken/bevoelkerung/geburten-todesfaelle/geburten.html">Bundesamt für Statistik</a>'
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Durchschnittliche kinderanzahl je Frau'
+            }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
+        },
+        series: [{
+            name: 'Durchschnittliche Kinderanzahl je Frau',
+            data: dataL
+        }, {
+            name: 'Ersatz der Elterngeneration',
+            data: resultErsatz
+        }]
+    });
 
+    chartBalken = Highcharts.chart('balkenContainer', {
+
+        chart: {
+            type: 'column'
+        },
+
+        title: {
+            text: 'Lebend- und Totgeburten pro Jahr in der Schweiz',
+            align: 'left'
+        },
+
+        xAxis: {
+            categories: ['Geburten']
+        },
+
+        yAxis: {
+            allowDecimals: false,
+            min: 0,
+            title: {
+                text: 'Anzahl'
+            }
+        },
+
+        series: [{
+            name: 'Lebendgeburt Mädchen',
+            data: [148],
+            stack: 'lebend'
+        }, {
+            name: 'Lebendgeburt Knaben',
+            data: [102],
+            stack: 'lebend'
+        }, {
+            name: 'Todgeburt',
+            data: [113],
+            stack: 'tot'
+        }],
+
+        tooltip: {
+            formatter: function () {
+                console.log(this.point)
+                maedchen = this.point.maedchen
+                knaben = this.point.knaben
+                return '<b>' + this.x + '</b><br/>' +
+                    this.series.name + ': ' + this.y + '<br/>' +
+                    'Total: ' + this.point.stackTotal;
+            }
+        },
+
+        plotOptions: {
+            column: {
+                stacking: 'normal'
+            }
+        }
+    });
 })();
-
